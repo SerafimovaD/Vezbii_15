0788bb823763ab0ac60ad271dc802c92f1070f7b
SerafimovaD <daci.serafimova@gmail.com>
2023-12-15 09:59:52 +0100

    Add first java program

---
+++ src/Minimum.java
@@ -0,0 +1,20 @@
+
+public class Minimum {
+	
+	public static int findMin(int n1, int n2, int n3) {
+		int min;
+		if(n1<n2)
+			min=n1;
+		else
+			if(n2<n3)
+			min=n2;
+			else
+				min=n3;
+		
+		return min;
+	}
+	public static void main(String[] args) {
+		System.out.println("Minimum od dvata broja e:" + findMin(10,5,1));
+	}
+
+}
